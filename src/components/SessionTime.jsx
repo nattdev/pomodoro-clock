000d452affca43cc74a6afdc652cca6249edb018
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-01-30 21:22:28 -0500

    Change the composition of currentTIme

--- src/components/SessionTime.jsx
+++ src/components/SessionTime.jsx
@@ -1,8 +1,15 @@
-function SessionTime({minutes, seconds, timerLabel}) {
+function SessionTime({minutes, seconds, isComplete}) {
+
+    const minutesTime = /^[0-9]$/.test(minutes) ? `0${minutes}` : `${minutes}`;
+    const secondsTime = /^[0-9]$/.test(seconds) ? `0${seconds}` : `${seconds}`;
+    const currentTime = `${minutesTime}:${secondsTime}`
+
+    console.log(currentTime, isComplete);
+
     return(
         <div className="session-container">
-            <p id="timer-label">{timerLabel}</p>
-            <p id="time-left">{/^[0-9]$/.test(minutes) ? `0${minutes}` : minutes}:{/^[0-9]$/.test(seconds) ? `0${seconds}` : seconds}</p>
+            <p id="timer-label">{isComplete ? "Break" : "Session"}</p>
+            <p id="time-left">{currentTime}</p>
         </div>
     )
 };
